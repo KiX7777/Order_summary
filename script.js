31cef75d82e717d1a6b0d185c9087e1634bc6cb0
KiX7777 <kristianboi202@gmail.com>
2022-12-03 20:00:46 +0100

    popup

--- script.js
+++ script.js
@@ -16,3 +16,45 @@ const closep = function () {
 
   // popup.classList.toggle('scale-out-center');
 };
+
+const pop = document.querySelectorAll('.pop');
+
+// pop.forEach((option) => {
+//   option.addEventListener(
+//     'click',
+//     function (e) {
+//       console.log(e.target);
+//       option.classList.add('active');
+//     },
+//     { capture: false }
+//   );
+// });
+
+const selectedPlan = document.querySelector('#selectedPlan');
+const selectedPrice = document.querySelector('#selectedPrice');
+
+popup.addEventListener(
+  'click',
+  function (e) {
+    // option.classList.remove('active');
+    pop.forEach((opcija) => {
+      // opcija.classList.remove('active');
+      opcija.classList.remove('active');
+    });
+    console.log(e.target);
+    if (e.target.classList.contains('pop')) {
+      selectedPlan.textContent = 'mmm';
+      e.target.classList.toggle('active');
+    }
+  },
+  { capture: false }
+);
+
+// pop.forEach((tab) => {
+//   tab.addEventListener('click', function (e) {
+//     e.target.classList.toggle('active');
+//     tab.classList.remove('active');
+//   });
+//   popup.classList.remove('active');
+//   e.target.classList.toggle('active');
+// });
