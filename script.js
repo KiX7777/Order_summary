d8ded95dca0d1885cfa7436eed0cca6c4a9f87ef
KiX7777 <kristianboi202@gmail.com>
2022-12-03 17:20:36 +0100

    added popup

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const change = document.querySelector('.change');
+const popup = document.querySelector('.popup');
+
+const openpopup = function () {
+  popup.classList.toggle('hidden');
+  popup.classList.toggle('scale-in-center');
+  popup.classList.remove('scale-out-center');
+};
+
+const closep = function () {
+  popup.classList.toggle('hidden');
+  popup.classList.toggle('scale-in-center');
+  popup.classList.add('scale-out-center');
+
+  // popup.classList.toggle('scale-out-center');
+};
